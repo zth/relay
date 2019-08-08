6a915f5e333e5b275eaba972722a0ed99fc85552
Gabriel Nordeborn <gabbe.nord@gmail.com>
2019-08-08 19:41:07 +0200

    Expose getFileFilter through language plugin, and allow passing it to RelaySourceModuleParser.

--- packages/relay-compiler/language/RelayLanguagePluginInterface.js
+++ packages/relay-compiler/language/RelayLanguagePluginInterface.js
@@ -15,6 +15,7 @@ const {RelayConcreteNode} = require('relay-runtime');
 import type {IsGeneratedFileFn} from '../codegen/CodegenRunner';
 import type {IRTransform} from '../core/GraphQLCompilerContext';
 import type {GeneratedDefinition, Root, Fragment} from '../core/GraphQLIR';
+import type {GetFileFilter} from '../core/RelaySourceModuleParser';
 import type {ScalarTypeMapping} from './javascript/RelayFlowTypeTransformers';
 import type {GeneratedNode} from 'relay-runtime';
 
@@ -38,6 +39,7 @@ export type PluginInterface = {
   typeGenerator: TypeGenerator,
   isGeneratedFile?: IsGeneratedFileFn,
   getModuleName?: (operationName: string) => string,
+  getFileFilter?: GetFileFilter,
 };
 
 /**
