8ac2e780547860117c5464bcccb885a070ac0362
Gabriel Nordeborn <gabbe.nord@gmail.com>
2025-10-26 16:16:34 +0100

    more output

--- compiler/crates/relay-typegen/src/rescript.rs
+++ compiler/crates/relay-typegen/src/rescript.rs
@@ -2114,8 +2114,7 @@ fn write_get_connection_nodes_function(
                             local_indentation += 1;
                             if edges_nullable {
                                 write_indentation(str, local_indentation).unwrap();
-                                writeln!(str, "->Belt.Array.keepMap(edge => switch edge {{")
-                                    .unwrap();
+                                writeln!(str, "->Array.filterMap(edge => switch edge {{").unwrap();
 
                                 write_indentation(&mut ending_str, local_indentation).unwrap();
                                 writeln!(ending_str, "}})").unwrap();
@@ -2128,7 +2127,7 @@ fn write_get_connection_nodes_function(
                                 write!(str, "| Some(edge) => ").unwrap();
                             } else {
                                 write_indentation(str, local_indentation).unwrap();
-                                writeln!(str, "->Belt.Array.keepMap(edge => ").unwrap();
+                                writeln!(str, "->Array.filterMap(edge => ").unwrap();
 
                                 write_indentation(&mut ending_str, local_indentation).unwrap();
                                 writeln!(ending_str, ")").unwrap();
